578e26c5756e5eda083aa1286840f5903e0b39d6
wowplayah <syafiiazami2013@gmail.com>
2018-07-19 22:06:06 +0700

    Update tambah soal
    Menambahkan sedikit template.

---
+++ Views/ManagementSoal/TambahSoal/TambahSoal.php
@@ -0,0 +1,36 @@
+<div class="container-fluid body-content">
+    
+    <div class="row" style="margin-bottom: 20px;">
+        <div class="col-sm-12">
+            <table class="table table-bordered table-hover table-striped">
+                <thead style="background: #22313F;color:#fff;">
+                    <tr>
+                    <th scope="col">KODE SOAL</th>
+                    <th scope="col">KODE UK</th>
+                    <th scope="col">NAMA MATA LATIHAN</th>
+                    </tr>
+                </thead>
+                <tbody style="cursor: pointer;">
+                    <tr data-toggle="collapse" data-target="#accordion" class="clickable">
+                        <td>1</td>
+                        <td>T.9007.001.002.02</td>
+                        <td>Membekali diri Tentang Kondisi Kerja dan Risiko Kerja</td>
+                    </tr>
+                    <tr  style="margin:-20px 0 -20px 0;">
+                        <td colspan="3">
+                            <div id="accordion" class="collapse">Hidden by default</div>
+                        </td>
+                    </tr>
+                    <tr>
+                    <td>2</td>
+                    <td>T.9007.001.003.02</td>
+                    <td>Menerapkan Prosedur Keselamatan dan Kesehatan Kerja di Tempat Kerja</td>
+                    </tr>
+                    <tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+<button class="btn btn-primary" style="margin:20px;position:absolute;z-index:9999;bottom:0;right:0;">TAMBAH BARU</button>
