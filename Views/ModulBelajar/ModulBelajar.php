13bbd1e6ac34e6020efcfa0404cd979042e16c15
Unknown <fadilnrsyh@gmail.com>
2018-07-25 21:24:51 +0700

    rapihin
    - kurikulum peserta
    - modul belajar
    - pendaftaran
    - test pendaftaran

--- Views/ModulBelajar/ModulBelajar.php
+++ Views/ModulBelajar/ModulBelajar.php
@@ -1,30 +1,42 @@
+
+
 <div class="container">
     <div class="row">
-        <div class="col-md-4">
+        <div class="col-md-12">
             <div class="card border-primary mb-3">
-                <div class="card-body">
-                    <h2>Bab 1</h2>
-                    <h3>Sub Bab</h3>
-                    <h5>Sub Sub Bab</h5>
-                    <h5>Sub Sub Bab</h5>
-                    <h2>Bab 2</h2>
-                    <h3>Sub Bab</h3>
-                    <h5>Sub Sub Bab</h5>
-                    <h5>Sub Sub Bab</h5>                   
-                </div>
-            </div>
-        </div>
-        <div class="col-md-8">
-             <div class="card border-primary mb-3">
-                <div class="card-body">
-                    <h2>What is Lorem Ipsum?</h2>
-                    <p><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                    </div><div>
-                    <h2>Why do we use it?</h2>
-                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-                    </div><br /><div>                  
+                <div class="card-header">
+                <h3>Modul - Pendidikan Bahasa Inggris untuk Public Area Attendant</h3>
                 </div>
             </div>
         </div>
     </div>
+   <div class="row">
+      <div class="col-md-3">
+         <div class="card border-primary mb-3">
+            <div class="card-body">
+               <h3>Bab 1</h3>
+               <h4>&nbsp;Sub Bab</h4>
+               <h5 class="text-primary">&nbsp;&nbsp;&nbsp;Sub Sub Bab</h5>
+               <h5>&nbsp;&nbsp;&nbsp;Sub Sub Bab</h5>
+               <h3>Bab 2</h3>
+               <h4>&nbsp;Sub Bab</h4>
+               <h5>&nbsp;&nbsp;&nbsp;Sub Sub Bab</h5>
+               <h5>&nbsp;&nbsp;&nbsp;Sub Sub Bab</h5>
+            </div>
+         </div>
+      </div>
+      <div class="col-md-9">
+         <div class="card border-primary mb-3">
+            <div class="card-body">
+               <h2>What is Lorem Ipsum?</h2>
+               <p><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+               <h2>Why do we use it?</h2>
+               <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+            </div>
+            <br />
+            <div>                  
+            </div>
+         </div>
+      </div>
+   </div>
 </div>
--- Views/Pendaftaran/Pendaftaran.php
+++ Views/Pendaftaran/Pendaftaran.php
@@ -4,63 +4,75 @@
             <div class="card border-primary mb-3">
                 <div class="card-header">Pendaftaran &nbsp;</div>
                 <div class="card-body">
-                    <div class="form-group">
-                        <label>No Induk</label>
-                        <input class="form-control" placeholder="70200000082" type="text">
-                    </div>
-                    <div class="form-group">
-                        <label>Negara Tujuan</label>
-                        <select class="form-control">
-                            <option>Indonesia</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label>Pekerjaan Yang Di Inginkan</label>
-                        <select class="form-control">
-                            <option>Housekeeper</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label>Nama Lengkap (Sesuai KTP)</label>
-                        <input class="form-control" placeholder="Nama Sesuai KTP" type="text">
-                    </div>
-                    <div class="form-group">
-                        <label>Tempat Lahir</label>
-                        <input class="form-control" placeholder="Tempat Lahir" type="text">
-                    </div>
-                    <div class="form-group">
-                        <label>Tanggal Lahir</label>
-                        <input class="form-control" placeholder="DD/MM/YYYY" type="text">
-                    </div>
-                    <div class="form-group">
-                        <label>Alamat Sekarang</label>
-                        <textarea class="form-control" placeholder="Tulis Alamat Lengkap"></textarea>
-                    </div>
-                    <div class="form-group">
-                        <label>Kabupaten</label>
-                        <select class="form-control">
-                            <option>Tangerang</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label>Kode Pos</label>
-                        <input class="form-control" placeholder="10001" type="text">
-                    </div>
-                    <div class="form-group">
-                        <label>Pendidikan Terakhir</label>
-                        <select class="form-control">
-                            <option>SMA/SMK</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label>Paspor (Optional)</label>
-                        <input class="form-control" placeholder="Paspor" type="text">
-                    </div>
-                    <div class="form-group">
-                        <label>Negara Tujuan Sebelumnya</label>
-                        <select class="form-control">
-                            <option>Arab Saudi</option>
-                        </select>
+                    <div class="col-sm-12 row">
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <label>No Induk</label>
+                                <input class="form-control form-control-sm" placeholder="70200000082" type="text">
+                            </div>
+                            <div class="form-group">
+                                <label>Negara Tujuan</label>
+                                <select class="form-control form-control-sm">
+                                    <option>Indonesia</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label>Pekerjaan Yang Di Inginkan</label>
+                                <select class="form-control form-control-sm">
+                                    <option>Housekeeper</option>
+                                    <option>Baby Sitter</option>
+                                    <option>Child care</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label>ID</label>
+                                <input class="form-control form-control-sm" placeholder="KTP/SIM" type="text">
+                            </div>
+                            <div class="form-group">
+                                <label>Nama Lengkap (Sesuai KTP)</label>
+                                <input class="form-control form-control-sm" placeholder="Nama Sesuai KTP" type="text">
+                            </div>
+                            <div class="form-group">
+                                <label>Tempat Lahir</label>
+                                <input class="form-control form-control-sm" placeholder="Tempat Lahir" type="text">
+                            </div>
+                            <div class="form-group">
+                                <label>Tanggal Lahir</label>
+                                <input class="form-control form-control-sm" placeholder="DD/MM/YYYY" type="text">
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <label>Alamat Sekarang</label>
+                                <textarea class="form-control form-control-sm" placeholder="Tulis Alamat Lengkap"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label>Kabupaten</label>
+                                <select class="form-control form-control-sm">
+                                    <option>Tangerang</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label>Kode Pos</label>
+                                <input class="form-control form-control-sm" placeholder="10001" type="text">
+                            </div>
+                            <div class="form-group">
+                                <label>Pendidikan Terakhir</label>
+                                <select class="form-control form-control-sm">
+                                    <option>SMA/SMK</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label>Paspor (Optional)</label>
+                                <input class="form-control form-control-sm" placeholder="Paspor" type="text">
+                            </div>
+                            <div class="form-group">
+                                <label>Negara Tujuan Sebelumnya</label>
+                                <select class="form-control form-control-sm">
+                                    <option>Arab Saudi</option>
+                                </select>
+                            </div>
+                        </div>
                     </div>
                     <a href="testpendaftaran" class="btn btn-primary">Daftar</a>
                 </div>
